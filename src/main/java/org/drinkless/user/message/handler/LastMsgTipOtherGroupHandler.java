d7c7ef08d9a2354ce4d96a9ab620d178657d6c43
github-dev <github-dev@abc.com>
2023-08-16 00:38:21 +0800

    join group tip

--- src/main/java/org/drinkless/user/message/handler/LastMsgTipOtherGroupHandler.java
+++ src/main/java/org/drinkless/user/message/handler/LastMsgTipOtherGroupHandler.java
@@ -20,27 +20,50 @@ public class LastMsgTipOtherGroupHandler implements LastMsgHandler{
         if (clientMainUser.receiveGroupId == 0 || clientMainUser.receiveGroupId == updateChat.chatId || sender.getConstructor() != TdApi.MessageSenderUser.CONSTRUCTOR) {
             return;
         }
-        long userId = ((TdApi.MessageSenderUser)sender).userId;
-        if (System.currentTimeMillis() - clientMainUser.userLastMsgTime.getOrDefault(userId, 0L) < TimeUnit.MINUTES.toMillis(clientMainUser.checkInterval)) {
-            return;
-        }
-        clientMainUser.userLastMsgTime.put(userId, System.currentTimeMillis());
-        TdApi.User user = clientMainUser.users.get(userId);
-        String firstName = Optional.ofNullable(user).map(u -> u.firstName).orElse("未知昵称");
-        String userName = Optional.ofNullable(user).map(u -> u.usernames).map(usernames -> usernames.activeUsernames).filter(names -> names.length > 0).map(names -> names[0]).orElse("未知用户名");
-        if (clientMainUser.noListenUserId.contains(userId) || clientMainUser.noListenUserName.contains(userName)) {
-            return;
+        long userId = 0;
+        String userName = "未知用户名";
+        String firstName = "未知昵称";
+        if (sender.getConstructor() == TdApi.MessageSenderUser.CONSTRUCTOR) {
+            userId = ((TdApi.MessageSenderUser)sender).userId;
+            if (System.currentTimeMillis() - clientMainUser.userLastMsgTime.getOrDefault(userId, 0L) < TimeUnit.MINUTES.toMillis(clientMainUser.checkInterval)) {
+                return;
+            }
+            clientMainUser.userLastMsgTime.put(userId, System.currentTimeMillis());
+            TdApi.User user = clientMainUser.users.get(userId);
+            firstName = Optional.ofNullable(user).map(u -> u.firstName).orElse("未知昵称");
+            userName = Optional.ofNullable(user).map(u -> u.usernames).map(usernames -> usernames.activeUsernames).filter(names -> names.length > 0).map(names -> names[0]).orElse("未知用户名");
+            if (clientMainUser.noListenUserId.contains(userId) || clientMainUser.noListenUserName.contains(userName)) {
+                return;
+            }
+        } else if (sender.getConstructor() == TdApi.MessageSenderChat.CONSTRUCTOR) {
+            userId = ((TdApi.MessageSenderChat)sender).chatId;
+            TdApi.Chat chat = clientMainUser.chats.get(userId);
+            if (chat != null) {
+                firstName = chat.themeName;
+                userName = chat.title;
+            }
         }
         TdApi.Chat room = clientMainUser.chats.get(updateChat.lastMessage.chatId);
         TdApi.MessageContent content = updateChat.lastMessage.content;
-        String msg;
+        StringBuilder msg;
         if (content.getConstructor() == TdApi.MessageText.CONSTRUCTOR) {
-            msg = "文字消息:\n" + ((TdApi.MessageText) content).text.text;
+            msg = new StringBuilder("文字消息:\n" + ((TdApi.MessageText) content).text.text);
+        } else if(content.getConstructor() == TdApi.MessageChatAddMembers.CONSTRUCTOR) {
+            msg = new StringBuilder("进群消息:\n");
+            for (long memberUserId : ((TdApi.MessageChatAddMembers) content).memberUserIds) {
+                TdApi.User member = clientMainUser.users.get(memberUserId);
+                String memberName = Optional.ofNullable(member).map(u -> u.firstName).orElse("未知昵称");
+                String memberUserName = Optional.ofNullable(member).map(u -> u.usernames).map(usernames -> usernames.activeUsernames).filter(names -> names.length > 0).map(names -> names[0]).orElse("未知用户名");
+                msg.append("\"").append(memberUserId).append("-").append(memberName).append("-").append(memberUserName).append("\"加入了群\n");
+            }
+        } else if(content.getConstructor() == TdApi.MessageChatJoinByLink.CONSTRUCTOR || content.getConstructor() == TdApi.MessageChatJoinByRequest.CONSTRUCTOR) {
+            msg = new StringBuilder("进群消息:\n");
+            msg.append("\"").append("未知id").append("-").append("未知昵称").append("-").append("未知用户名").append("\"加入了群\n");
         } else {
-            msg = "非文字消息:\n" + content.getClass().getSimpleName();
+            msg = new StringBuilder("非文字消息:\n" + content.getClass().getSimpleName());
         }
-        msg = "\""+ userId + "-" + firstName + "-" + userName + "\", 在\"" + room.title + "\"中说话\n" + msg;
-        TdApi.FormattedText text = new TdApi.FormattedText(msg, null);
+        msg.insert(0, "\"" + userId + "-" + firstName + "-" + userName + "\", 在\"" + room.title + "\"中说话\n");
+        TdApi.FormattedText text = new TdApi.FormattedText(msg.toString(), null);
         TdApi.InputMessageContent sendContent = new TdApi.InputMessageText(text, false, true);
         clientMainUser.client.send(new TdApi.SendMessage(clientMainUser.receiveGroupId, 0, 0, null, null, sendContent), obj -> {});
     }
